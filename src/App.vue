7719f73038b09a0eccaae19b727c690c2360c037
Conor Walsh <cewalsh@bu.edu>
2020-03-23 22:29:08 -0400

    Fixed Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app>
     <v-content>
+      <router-link to="/login">Go to Login</router-link>
       <router-view>
-        <router-link to="/login">Go to Login</router-link>
-      </router-view>
-       
+      </router-view> 
     </v-content>
   </v-app>
 </template>
