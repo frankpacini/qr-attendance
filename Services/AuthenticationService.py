eb0d17699361eb825305bb6c2957284178d1fa1f
Conor Walsh <cewalsh@bu.edu>
2020-03-23 22:40:14 -0400

    Added a lot of stuff

---
+++ Services/AuthenticationService.py
@@ -0,0 +1,121 @@
+from flask import Blueprint
+from flask import current_app as app 
+from flask import request, redirect, url_for
+from functools import wraps
+from uuid import uuid4
+from Models.User import User
+from datetime import datetime
+
+import uuid
+import hashlib
+
+class AuthenticationService():
+    
+    def __init__(self):
+        return
+
+    """ 
+    Return the user
+    """ 
+    def getUser(self, **kwargs):
+        if not User.objects(**kwargs):
+            return False
+
+        user = User.objects.get(**kwargs) #use objects.get to retreive one result
+        
+        return user
+
+
+
+    """
+    Authenticate the user (Login). 
+    @param: email 
+    @param: password
+    @return: unique session id
+    """
+
+    def authenticate(self,email,password):
+        hashedPassword = self.saltPassword(password)
+        user = self.getUser(email=email)
+
+        if not user:
+            return False
+
+        if hashedPassword != user.password:
+            return False
+
+        sessionId = uuid4()
+        session = Session(user=user, sessionId=sessionId)
+        session.save()
+        return session
+    
+    """
+    Save the user to the database upon signup if they don't exist
+    """
+    def save(self, user): 
+        if self.getUser(email=user.email): 
+            return False
+        else:
+            user.password = self.saltPassword(user.password)
+            user.save()
+            return True
+          
+    """
+     Takes a password and returns a hashed version of it
+    """
+    def saltPassword(self, password):
+
+        yummyYummySalty = "dHw33Th"
+        db_password = password+yummyYummySalty
+        hasher = hashlib.sha256(db_password.encode())
+        hashLevelOne = hasher.hexdigest()
+        supaHasher = hashlib.sha256(hashLevelOne.encode())
+        hashLevelTwo = supaHasher.hexdigest()
+        
+        return hashLevelTwo
+     
+    """
+    Currently, all users will not be administrators.
+    """
+    def signup(self, document):
+        user = User(
+            firstName=document["firstName"], 
+            lastName=document["lastName"], 
+            email=document["email"],
+            password=document["password"]
+            )
+    
+        user.validate()  # TODO: enclose this in a try/catch block /check if its an error with the type entered
+
+        if (self.save(user)):
+            return True
+        else:
+            return False
+
+    def emailIsAlreadyInUse(self, email):
+        return User.objects(email=email)
+        
+    def changeEmail(self, oldEmail, newEmail):
+        User.objects.get(email=oldEmail).update(email=newEmail)
+
+    def changePassword(self, email, password):
+        User.objects.get(email=email).update(password=self.saltPassword(password))   
+
+    def getSession(self, sessionId):
+        sessionUUID = uuid.UUID(sessionId)
+        return Session.objects.get(sessionId=sessionUUID)
+
+    def logout(self, sessionId):
+        session = self.getSession(sessionId)
+        if (session):
+            session.delete()
+        else:
+            raise
+            
+    
+    def verifySessionAndReturnUser(self, sessionId):
+        session = self.getSession(sessionId)
+        if (datetime.utcnow() < session.dateExpires):
+            return User.objects.get(id=session.user.id)
+        else:
+            return False
--- application.py
+++ application.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from mongoengine import connect
+import yaml
+
+# Instantiate connection to database
+creds = yaml.safe_load(open("creds.yaml", "r"))
+dbHostUri = "mongodb+srv://" + creds["DB_USER"] + ":" + creds["DB_PASSWORD"] + \
+    "@cluster0-ollas.mongodb.net/test?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+db = connect(host=dbHostUri)
+
 application = Flask(__name__)
 
 @application.route('/')
@@ -6,14 +15,13 @@ def hello_world():
     return 'Hello, World!'
 
 def importControllers():
-    print("Hello")
-    #with application.app_context():
+    with application.app_context():
         # import Controllers.AdminController as admin
-        # import Controllers.AuthenticationController as auth
+        import Controllers.AuthenticationController as auth
         # import Controllers.UploadController as upload
         # import Controllers.DatasetController as dataset
 
-        # application.register_blueprint(admin.admin)
+        application.register_blueprint(admin.admin)
         # application.register_blueprint(auth.auth)
         # application.register_blueprint(upload.upload)
         # application.register_blueprint(dataset.dataset)
