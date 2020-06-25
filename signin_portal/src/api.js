62f5caf239f0d20c00887dad21634c67415d0069
fgpacini <fgpacini@yahoo.com>
2020-06-25 19:36:42 -0400

    Moved portal files

---
+++ signin_portal/src/api.js
@@ -0,0 +1,80 @@
+import { getCookie, deleteCookie } from "./js/authentication";
+import router from "./router";
+import { post,//,get, _delete 
+get
+} from "./requests";
+import store from "./store"; // might be a circular import
+import notify from "./utilities/notify";
+import { colors } from "./utilities/branding";
+
+const api = {
+  getNames(meetingID){
+    return get('/meeting/names/' + meetingID)
+  },
+  setName(meetingID, name, userID){
+    return post("/attendance/" + meetingID, {name: name, userID: userID})
+  },
+  createMeeting(name) {
+    console.log("This was called")
+    return post("/meeting/createMeeting", {name: name});
+  },
+  checkMeeting(meetingID) {
+    return get("/meeting/" + meetingID);
+  },
+  closeMeeting(meetingID) {
+    return post("/meeting/" + meetingID, false);
+  },
+  logout() {
+    post("/auth/logout", { sessionId: getCookie("SID") })
+      .then(() => {
+        deleteCookie("SID");
+        store.commit("setLoggedInFalse");
+        router.push("/");
+        notify("User logged out", colors.green);
+      })
+      .catch(() => {
+        notify("Unable to logout. Please try again later. ", colors.red);
+      });
+  },
+  getSessionID(username, password) {
+    post("/auth/login", { email: username, password: password })
+      .then(() => {
+        console.log("Hi")
+        /*
+        if (response.data.admin == true) {
+          store.commit("setIsAdmin");
+        }
+        */
+        store.commit("setUser", username);
+        store.commit("setLoggedInTrue");
+        router.push("/home");
+        notify("Successfully logged in", colors.green);
+      })
+      .catch(() => {
+        notify("Incorrect username or password. Please try again", colors.red);
+      });
+  },
+  verifyLogin() {
+    const sessionId = getCookie("SID");
+    if (!sessionId) {
+      store.commit("setLoggedInFalse");
+      router.push("/login");
+      return false;
+    }
+    return post("/auth/verifySession", { sessionId: sessionId })
+      .then(() => {
+        store.commit("setLoggedInTrue");
+        router.push("/home");
+        return true;
+      })
+      .catch((error) => {
+        deleteCookie("SID");
+        store.commit("setLoggedInFalse");
+        router.push("/login");
+        notify(error.response.data.message, colors.red);
+        return false;
+      })
+  },
+};
+
+export default api;
