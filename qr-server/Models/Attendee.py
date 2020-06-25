1fc4a27400653f9c257dabd6cfb5e670923a8d4f
fgpacini <fgpacini@yahoo.com>
2020-06-25 19:48:28 -0400

    Moved platform

---
+++ qr-server/Models/Attendee.py
@@ -0,0 +1,9 @@
+from mongoengine import *
+import uuid
+
+class Attendee(DynamicDocument):
+    name = StringField(required=True)
+    attendeeID = UUIDField(binary=False, required=True, default=uuid.uuid4, unique=True)    
+    meetings = ListField(ReferenceField('Meeting'))
+
+        
